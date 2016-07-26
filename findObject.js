8cb93122e90e8763630b8feecede1f1eb876be53
charlieperson <charlie.person@hyperisland.se>
2016-07-26 09:56:51 -0500

    Solves problem using non-mutative recursion

--- findObject.js
+++ findObject.js
@@ -1,6 +1,21 @@
+// for loop
+
 function findPerson (name, people) {
   for(var i=0; i<people.length; i++){
     if(people[i].name === name) return people[i].hobby;
   }
   return false;
+}
+
+// recursion
+
+function findPerson (name, people) {
+  var i=0;
+  function recurse(item){
+    console.log('person', people[i]);
+    if(i >= people.length) return false;
+    if(people[i].name === name) return people[i].hobby;
+    return recurse(people[i+=1]);
+  }
+  return recurse(people[0]);
 }
